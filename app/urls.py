1bab072c58c68c9a160659cd6195a95dc08b3f79
lizhilin014 <lizhilin014@ke.com>
2025-05-27 16:51:51 +0800

    清理demo接口

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
 
-from app.controllers import rag, health
-from app.views import main
+from app.controllers import rag, health, chunk, file, qa
 
 urlpatterns = [
-    url(r'^$', main.index, name="index"),
-    url(r'^mysqltestdemo$', main.mysqltestdemo, name="mysqltestdemo"),
-    url(r'^errordemo$', main.errordemo, name="errordemo"),
 
     url(r'^file/indexing$', rag.file_indexing, name="file_indexing"),
     url(r'^rag/rag_query$', rag.rag_query, name="rag_query"),
