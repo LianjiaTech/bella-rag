b665801112d858f466e77b7783007b88eeee6cba
lizhilin014 <lizhilin014@ke.com>
2025-05-30 14:48:31 +0800

    无traceid逻辑修正

--- ke_rag/utils/trace_log_util.py
+++ ke_rag/utils/trace_log_util.py
@@ -28,12 +28,42 @@ def trace_log(step):
                 result = func(*args, **kwargs)
                 end = int(time.time() * 1000)
                 cost = end - start
-                log = build_trace_json(step, trace_context.get(), cost, start, result, None, *args, **kwargs)
-                logger.info(log)
+                rag_histogram.labels(step).observe(cost)
+                user_logger.info(f'rag step : {step} time cost : {cost}')
+                payload = {
+                    "step": step,
+                    "status": status,
+                    "trace_id": trace_id,
+                    "message": message,
+                    "cost": cost,
+                    "result": result,
+                    "log_enabled": log_enabled,
+                    "args": args,
+                    "kwargs": kwargs
+                }
+                # event不支持自定义，在payload传递真正的step
+                callback_manager.on_event_end(CBEventType.AGENT_STEP, payload)
+
+            start = int(time.time() * 1000)
+            trace_id = TraceContext.trace_id
+            if not trace_id:
+                # 上下文找不到trace_id则直接返回
+                res = func(*args, **kwargs)
+                rag_histogram.labels(step).observe(int(time.time() * 1000) - start)
+                user_logger.info(f'rag step : {step} time cost : {int(time.time() * 1000) - start}')
+                return res
+
+            try:
+                user_logger.info(f"trace step:{step}, trace id:{trace_id}")
+                callback_manager.on_event_start(CBEventType.AGENT_STEP, {"step": step, "trace_id": trace_id})
+                result = func(*args, **kwargs)
+                send_event("success", '', **kwargs, result=result)
+                return result
+            except BusinessError as be:
+                send_event("failed", be.error_msg)
+                raise
             except Exception:
-                mg = traceback.format_exc()
-                log = build_trace_json(step, trace_context.get(), cost, start, result, mg, *args, **kwargs)
-                logger.info(log)
+                send_event("failed", "internal error")
                 raise
             return result
 
